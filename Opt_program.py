afd0871f21692103484fc30f0531e0bb94428852
Ben Conlon <benjamin.conlon@gmail.com>
2016-01-27 15:51:59 -0500

    Create Opt_program.py

---
+++ Opt_program.py
@@ -0,0 +1,7 @@
+"""
+This is and attempt to port over an existing solar optimization program into python.
+eventually, it will be more complex than the originial program, and have a GUI.
+The goal being to eventually have a way to draw a layout for fingers on a solar cell, and 
+immediatly calculate the resistance.
+"""
+
