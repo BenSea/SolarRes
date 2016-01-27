f3ebb71343f42b58913523838458a15cf247bb4f
Ben Conlon <benjamin.conlon@gmail.com>
2016-01-27 18:21:53 -0500

    Update Opt_program.py

--- Opt_program.py
+++ Opt_program.py
@@ -5,3 +5,18 @@ The goal being to eventually have a way to draw a layout for fingers on a solar 
 immediatly calculate the resistance.
 """
 
+#Variable Unit Conversions
+Ang = 1e-10 # Angstrom unit in meters
+nm = 1e-9 #Nanometer unit in meters
+um = 1e-6 #Micron unit in meters
+mm = 1e-3 #Milimeter unit in meters
+cm = 1e-2 #centimeter unit in meters
+
+
+
+# Instantiate CONSTANTS AND STANDARD PARAMATERS FOR CALCULATIONS
+rho_au = 2.44e-8 #Resistivity of gold in [ohm-m] @ 20 C
+q = 1.602e-19 # Coulomg unit of charge
+N_n =
+
+
